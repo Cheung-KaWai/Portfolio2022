b2ec6db96bc23e3e3817d05a5e194030956a68b8
Cheung-KaWai <kawaicheungdev@gmail.com>
2022-04-12 10:15:31 +0200

    Modify animations introduction and fix typo

--- src/components/Home/Introduction.jsx
+++ src/components/Home/Introduction.jsx
@@ -8,7 +8,7 @@ export default function Introduction() {
   return (
     <Intro>
       Hi, my name is Ka Wai cheung.
-      <br /> I'm a student <br /> <Highlight>Web & Mobile</Highlight>{" "}
+      <br /> I'm a student <br /> <Highlight>Web & Mobile </Highlight>
       development <Blob>:D</Blob>
     </Intro>
   );
@@ -19,15 +19,25 @@ const Intro = styled.h1`
   font-size: ${sizes.h1};
 `;
 
-const background = keyframes`
+const slide = keyframes`
   0%{background-position:right}
   100%{background-position: left}
+`;
+
+const bounce = keyframes`
+  0%{background-size:0% 0%}
+  50%{background-size: 130% 130%}
+  60%{background-size: 100% 100%}
+  70%{background-size: 120% 120%}
+  80%{background-size: 100% 100%}
+  90%{background-size: 110% 110%}
+  100%{background-size: 100% 100%}
 `;
 
 const Highlight = styled.span`
   background: linear-gradient(-60deg, #fff 50%, ${colors.lightGreen} 50% 100%);
   background-size: 160% 200%;
-  animation: ${background};
+  animation: ${slide};
   animation-timing-function: ease-in;
   animation-duration: 1s;
   animation-fill-mode: forwards;
@@ -39,4 +49,10 @@ const Blob = styled.span`
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
+  background-size: 0% 0%;
+  animation: ${bounce};
+  animation-timing-function: ease-out;
+  animation-delay: 1s;
+  animation-duration: 2s;
+  animation-fill-mode: forwards;
 `;
